bf3993df76c62e045a11daaf019bf639d71886ed
shafiqbd1st <shafiqbd1st.com>
2024-03-12 05:55:24 +0000

    Tue, Mar 12, 2024, 11:55 AM +06:00

--- module 12/fifth_project/first_app/forms.py
+++ module 12/fifth_project/first_app/forms.py
@@ -1,22 +1,22 @@
 from django import forms
 
-# class contactForm(forms.Form):
-#     Name = forms.CharField(
-#         max_length=200,
-#         label="User name",
-#         widget=forms.TextInput(attrs={"placeholder": "Enter your name"}),
-#     )
-#     Email = forms.EmailField(max_length=200, label="User Email",widget=forms.TextInput(attrs={'placeholder': "Enter your Email"} ))
-#     Age = forms.IntegerField(required=False)
-#     Gender = forms.ChoiceField(choices=[('m','male'), ('f', 'female')])
-#     birthday = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
-#     value = [('s', 'small'), ('m', 'medium'), ('l', 'large')]
-#     Size = forms.ChoiceField(choices=value, widget=forms.RadioSelect)
-#     meal = [('p', 'pizza'), ('b', 'burger'), ('s','sandwich'), ('c', 'cheese')]
-#     Meal = forms.MultipleChoiceField(choices=meal, widget=forms.CheckboxSelectMultiple)
+class contactForm(forms.Form):
+    Name = forms.CharField(
+        max_length=200,
+        label="User name",
+        widget=forms.TextInput(attrs={"placeholder": "Enter your name"}),
+    )
+    Email = forms.EmailField(max_length=200, label="User Email",widget=forms.TextInput(attrs={'placeholder': "Enter your Email"} ))
+    Age = forms.IntegerField(required=False)
+    Gender = forms.ChoiceField(choices=[('m','male'), ('f', 'female')])
+    birthday = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
+    value = [('s', 'small'), ('m', 'medium'), ('l', 'large')]
+    Size = forms.ChoiceField(choices=value, widget=forms.RadioSelect)
+    meal = [('p', 'pizza'), ('b', 'burger'), ('s','sandwich'), ('c', 'cheese')]
+    Meal = forms.MultipleChoiceField(choices=meal, widget=forms.CheckboxSelectMultiple)
 
-#     Message = forms.CharField(max_length=200, widget=forms.Textarea)
-#     # file = forms.FileField()
+    Message = forms.CharField(max_length=200, widget=forms.Textarea)
+    # file = forms.FileField()
 
 class studentData(forms.Form):
     name = forms.CharField(widget = forms.TextInput)
