b6d4aa11a76f949d3c7aba48ceeee94fc259db15
shafiqbd1st <shafiqbd1st.com>
2024-03-12 05:54:33 +0000

    Tue, Mar 12, 2024, 11:54 AM +06:00

--- module 12/fifth_project/first_app/urls.py
+++ module 12/fifth_project/first_app/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.home, name = 'home'),
     path('about/', views.about, name = 'about'),
     path('form/', views.form, name = 'form'),
-    # path('django_form/', views.djangoForm, name = 'django_form'),
+    path('django_form/', views.djangoForm, name = 'django_form'),
     path('django_form/', views.studentData, name = 'django_form'),
 ]
