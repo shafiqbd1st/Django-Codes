89de76165539efedb85785a91b08685fab6aa65e
shafiqbd1st <shafiqbd1st.com>
2024-03-12 07:15:16 +0000

    Tue, Mar 12, 2024, 1:15 PM +06:00

--- module 12/fifth_project/first_app/forms.py
+++ module 12/fifth_project/first_app/forms.py
@@ -42,4 +42,4 @@ class contactForm(forms.Form):
 class studentData(forms.Form):
     name = forms.CharField(validators=[validators.MinLengthValidator(10, message="Please enter at least 10 characters")])
     email = forms.CharField(widget=forms.EmailInput, validators=[validators.EmailValidator(message="Please enter a valid email address")])
-    age = forms.IntegerField(validators=[validators.MaxValueValidator(35, message="Age must be maximum 35"), validators.MinValueValidator(30, message="Age must be minimum 20")])
+    age = forms.IntegerField(validators=[validators.MaxValueValidator(35, message="Age must be maximum 35"), validators.MinValueValidator(30, message="Age must be minimum 30")])
