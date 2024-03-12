7f765aa1c0d7a2b73a83525043926dec372413f6
shafiqbd1st <shafiqbd1st.com>
2024-03-12 04:31:05 +0000

    Tue, Mar 12, 2024, 10:31 AM +06:00

--- module 12/fifth_project/first_app/forms.py
+++ module 12/fifth_project/first_app/forms.py
@@ -3,12 +3,13 @@ from django import forms
 class contactForm(forms.Form):
     Name = forms.CharField(max_length=200, label="User name", widget=forms.TextInput(attrs={'placeholder': "Enter your name"} ))
     Email = forms.EmailField(max_length=200, label="User Email",widget=forms.TextInput(attrs={'placeholder': "Enter your Email"} ))
-    # Age = forms.IntegerField()
-    # Gender = forms.ChoiceField(choices=[('m','male'), ('f', 'female')])
+    Age = forms.IntegerField()
+    Gender = forms.ChoiceField(choices=[('m','male'), ('f', 'female')])
     # value = [('s', 'small'), ('m', 'medium'), ('l', 'large')]
     # Size = forms.ChoiceField(choices=value)
     # meal = [('p', 'pizza'), ('b', 'burger'), ('s','sandwich'), ('c', 'cheese')]
     # Meal = forms.MultipleChoiceField(choices=meal)
     # Check = forms.BooleanField()
     # Message = forms.CharField(widget=forms.Textarea)
-    file = forms.FileField()
+    birthday = forms.DateField(widget=forms.DateInput(attrs={'type': 'datetime'}))
+    # file = forms.FileField()
