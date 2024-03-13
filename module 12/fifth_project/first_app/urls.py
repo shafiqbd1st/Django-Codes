9277ef4048a0449a125c71b651f2fd4da95e7dab
shafiqbd1st <shafiqbd1st.com>
2024-03-13 07:04:04 +0000

    Wed, Mar 13, 2024, 1:04 PM +06:00

--- module 12/fifth_project/first_app/urls.py
+++ module 12/fifth_project/first_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .import views
+from . import views
+
 urlpatterns = [
     path("", views.home, name="homepage"),
-    path("about/", views.about, name="aboutpage"),
-    path("form/", views.form, name="form"),
-    path("djangoForm/", views.djangoForm, name="django_form"),
+    path("about/", views.about, name="about"),
+    path("form/", views.form, name="formpage"),
+    path("djangoForm/", views.djangoForm, name="DjangoForm"),
 ]
