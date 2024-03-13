cf31cbdca4addad17e605b2e64a00291db684d24
shafiqbd1st <shafiqbd1st.com>
2024-03-13 04:42:19 +0000

    Wed, Mar 13, 2024, 10:42 AM +06:00

--- Week 04 Conceptual Session 01/project/project/settings.py
+++ Week 04 Conceptual Session 01/project/project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'first_app',
     
 ]
 
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,7 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_DIRS = [
+    STACTIC/''
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
