cffbf5c6ed070142c5a47932aa259d62d11d9f86
shafiqbd1st <shafiqbd1st.com>
2024-03-14 05:34:10 +0000

    Thu, Mar 14, 2024, 11:34 AM +06:00

--- module 12/fifth_project/first_app/views.py
+++ module 12/fifth_project/first_app/views.py
@@ -1,25 +1,31 @@
 from django.shortcuts import render
-from . forms import ContactForm
+from .forms import ContactForm
+
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    return render(request, "home.html")
+
 
 def about(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         print(request.POST)
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        select = request.POST.get('select')
-        return render(request, 'about.html', {'name': name, 'email': email, 'select': select})
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        select = request.POST.get("select")
+        return render(
+            request, "about.html", {"name": name, "email": email, "select": select}
+        )
     else:
         return render(request, "about.html", {"name": "None", "email": "None"})
+
+
 def form(request):
-    return render(request, 'form.html')
+    return render(request, "form.html")
 
 
 def djangoForm(request):
     form = ContactForm()
     if form.is_valid():
         print(form.cleaned_data)
-    return render(request, 'django.html', {'form': form})
+    return render(request, "django.html", {"form": form})
