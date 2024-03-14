16a639770c86eefec412ade9fa695da9d147ad03
shafiqbd1st <shafiqbd1st.com>
2024-03-14 16:52:33 +0000

    Thu, Mar 14, 2024, 10:52 PM +06:00

--- module 12/fifth_project/first_app/forms.py
+++ module 12/fifth_project/first_app/forms.py
@@ -4,3 +4,6 @@ from django import forms
 class ContactForm(forms.Form):
     name = forms.CharField(label="User Name")
     email = forms.EmailField(label="User Email")
+    age = forms.IntegerField()
+    weight = forms.FloatField()
+    check = forms.BooleanField()
