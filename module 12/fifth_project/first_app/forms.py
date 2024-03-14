7965e74d5b2fde5ee5ae4864f4e55b0865c5ca38
shafiqbd1st <shafiqbd1st.com>
2024-03-14 16:39:54 +0000

    Thu, Mar 14, 2024, 10:39 PM +06:00

--- module 12/fifth_project/first_app/forms.py
+++ module 12/fifth_project/first_app/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    name = forms.CharField(max_length=100, label="User Name")
+    name = forms.CharField(max_length=50, label="User Name")
     email = forms.EmailField(label="User Email")
