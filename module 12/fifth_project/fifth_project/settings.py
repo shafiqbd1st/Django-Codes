06002a80ab072a56173aed93736806b591d3e11b
shafiqbd1st <shafiqbd1st.com>
2024-03-14 05:57:03 +0000

    Thu, Mar 14, 2024, 11:57 AM +06:00

--- module 12/fifth_project/fifth_project/settings.py
+++ module 12/fifth_project/fifth_project/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     "first_app",
 ]
 
-CRISPY_ALLOWED_TEMPLATE_PACK = "bootstrap5"
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
