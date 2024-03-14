e2dd8a9ef4c10e71be21f2aff42c3167e22d1ccc
shafiqbd1st <shafiqbd1st.com>
2024-03-14 05:55:14 +0000

    Thu, Mar 14, 2024, 11:55 AM +06:00

--- module 12/fifth_project/fifth_project/settings.py
+++ module 12/fifth_project/fifth_project/settings.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
     "first_app",
 ]
 
+CRISPY_ALLOWED_TEMPLATE_PACK = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
