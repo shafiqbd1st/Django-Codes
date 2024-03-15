72a921dcda27f07a30dead0dc00d6056236e7e27
shafiqbd1st <shafiqbd1st.com>
2024-03-15 05:16:41 +0000

    Fri, Mar 15, 2024, 11:16 AM +06:00

--- module 12/fifth_project/first_app/forms.py
+++ module 12/fifth_project/first_app/forms.py
@@ -2,7 +2,10 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    name = forms.CharField(label="User Name", required=False)
+    name = forms.CharField(
+        label="User Name",
+        required=False,
+    )
     # email = forms.EmailField(label="User Email")
     # age = forms.IntegerField()
     # weight = forms.FloatField()
@@ -13,5 +16,7 @@ class ContactForm(forms.Form):
     # Meals = forms.MultipleChoiceField(choices=CHOICES1)
     country = forms.CharField(initial="Bangladesh", disabled=True)
     # CV = forms.FileField()
-    message = forms.CharField(widget =forms.Textarea)
+    message = forms.CharField(
+        widget=forms.Textarea(attrs={"placeholder": "Enter your message"})
+    )
     # check = forms.BooleanField()
