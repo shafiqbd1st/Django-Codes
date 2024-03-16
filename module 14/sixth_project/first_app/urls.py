7e4582ad81416312536b5d7c38702e605725bf6f
shafiqbd1st <shafiqbd1st.com>
2024-03-16 16:59:10 +0000

    Sat, Mar 16, 2024, 10:59 PM +06:00

--- module 14/sixth_project/first_app/urls.py
+++ module 14/sixth_project/first_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from .import views
 urlpatterns = [
-    path("", views.home, "homepage"),
+    path("", views.home, name = "homepage"),
     path("delete/<int:roll>", views.delete_student, name="delete_student"),
 ]
