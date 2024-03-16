daa7aa9817850697bf459d4c81af0dad7a715cd6
shafiqbd1st <shafiqbd1st.com>
2024-03-16 16:53:54 +0000

    Sat, Mar 16, 2024, 10:53 PM +06:00

--- module 14/sixth_project/first_app/urls.py
+++ module 14/sixth_project/first_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from .import views
 urlpatterns = [
-    path('',views.home)
+    path('',views.home, 'homepage'),
+    path('delete/<int:roll>', views.delete_student, name = 'delete_student')
 ]
--- module 14/sixth_project/first_app/views.py
+++ module 14/sixth_project/first_app/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .import models
 # Create your views here.
 def home(request):
     student = models.Student.objects.all()
     print(student)
     return render(request,'home.html', {'data': student})
+
+def delete_student(request, roll):
+    student = models.Student.objects.get(pk = roll).delete()
+    print(student)
+    return redirect("homepage")
