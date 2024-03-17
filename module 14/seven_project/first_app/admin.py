cc3356ab30cceacf805e1c2eab3fb2ab6c9f4814
shafiqbd1st <shafiqbd1st.com>
2024-03-17 03:58:07 +0000

    Sun, Mar 17, 2024, 9:58 AM +06:00

--- module 14/seven_project/first_app/admin.py
+++ module 14/seven_project/first_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 
 # Register your models here.
-from . import models
-admin.site.register(models.StudentForm)
+from first_app.models import studentModel
+admin.site.register (studentModel)
