97744d3b057234301dcc32f076c4055696db6a50
shafiqbd1st <shafiqbd1st.com>
2024-03-17 03:39:56 +0000

    Sun, Mar 17, 2024, 9:39 AM +06:00

--- module 14/seven_project/seven_project/settings.py
+++ module 14/seven_project/seven_project/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'crispy_bootstrap'
     'first_app'
 ]
-CRISPY_ALLOWED_TEMPLATE_PACK = "bootstrap5"
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK ='bootstrap5'
 
 MIDDLEWARE = [
