520942c2cd03baf4211ebbdee185fcc9fdd4c4b4
shafiqbd1st <shafiqbd1st.com>
2024-03-17 06:16:18 +0000

    Sun, Mar 17, 2024, 12:16 PM +06:00

--- module 14/demoProject/first_app/models.py
+++ module 14/demoProject/first_app/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
-class StudentModel(models.Model):
+class studentModle(models.Model):
     roll = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=20)
     age = models.IntegerField()    
