c16ac84d124734f9055411e3f0f4f49736389f87
shafiqbd1st <shafiqbd1st.com>
2024-03-17 03:35:17 +0000

    Sun, Mar 17, 2024, 9:35 AM +06:00

--- module 14/seven_project/first_app/views.py
+++ module 14/seven_project/first_app/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 from first_app.forms import studentForm
 # Create your views here.
 def home(request):
-    stu = studentForm()
-    return render(request, 'home.html', {'form': stu})
+    
+    if request.method == 'POST':
+        form = studentForm(request.POST)
+        if form.is_valid():
+            form.save()
+        else:
+            form = studentForm()
+    return render(request, 'home.html', {'form': form})
