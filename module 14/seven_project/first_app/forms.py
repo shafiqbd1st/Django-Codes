ace1e6cb72151d37aed79219b2670ad9c7667987
shafiqbd1st <shafiqbd1st.com>
2024-03-17 06:46:40 +0000

    Sun, Mar 17, 2024, 12:46 PM +06:00

--- module 14/seven_project/first_app/forms.py
+++ module 14/seven_project/first_app/forms.py
@@ -1,11 +1,8 @@
 from django import forms
 from first_app.models import studentModel
 
+
 class studentForm(forms.ModelForm):
     class Meta:
         model = studentModel
-        fields = '__all__'
-        labels = {
-            'name': 'Student Name',
-            'roll': 'Student Roll'
-        }
+        fields = "__all__"
