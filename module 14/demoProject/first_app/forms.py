0cd8998082319611407ac8c75f1fa2cc6f28f293
shafiqbd1st <shafiqbd1st.com>
2024-03-17 06:15:19 +0000

    Sun, Mar 17, 2024, 12:15 PM +06:00

--- module 14/demoProject/first_app/forms.py
+++ module 14/demoProject/first_app/forms.py
@@ -1,2 +1,7 @@
 from django import forms
-from first_app.models import
+from first_app.models import studentModle
+
+class studentForm(forms.ModelForm):
+    class Meta:
+        model = studentModle
+        fields = '__all__'
