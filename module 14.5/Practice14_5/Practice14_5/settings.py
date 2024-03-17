098aaca7d7fb26329a9d0ae54b15bbbf363f640d
shafiqbd1st <shafiqbd1st.com>
2024-03-17 10:34:49 +0000

    Sun, Mar 17, 2024, 4:34 PM +06:00

--- module 14.5/Practice14_5/Practice14_5/settings.py
+++ module 14.5/Practice14_5/Practice14_5/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Practice14_5.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,4 +121,4 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.model
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
