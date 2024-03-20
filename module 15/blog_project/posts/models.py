b602ddd631356741d0719bea69e5530e2af3e016
shafiqbd1st <shafiqbd1st.com>
2024-03-20 16:34:28 +0000

    Wed, Mar 20, 2024, 10:34 PM +06:00

--- module 15/blog_project/posts/models.py
+++ module 15/blog_project/posts/models.py
@@ -2,6 +2,12 @@ from django.db import models
 
 # Create your models here.
 from author.models import Author
-from categories.models import categories
+from categories.models import Category
 
-
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    category = models.ManyToManyField(Category)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    
+    
--- module 15/blog_project/profiles/models.py
+++ module 15/blog_project/profiles/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 # Create your models here.
-from author.models import author
+from author.models import Author
 
 class profile(models.Model):
     name = models.CharField(max_length=100) 
     about = models.TextField()
-    author = models.OneToOneField(author, on_delete=models.CASCADE, default=None)
+    author = models.OneToOneField(Author, on_delete=models.CASCADE, default=None)
