b7d184f6236a0d3e08e112166b79e462f0e5126b
shafiqbd1st <shafiqbd1st.com>
2024-03-20 16:28:43 +0000

    Wed, Mar 20, 2024, 10:28 PM +06:00

--- module 15/blog_project/profiles/models.py
+++ module 15/blog_project/profiles/models.py
@@ -1,4 +1,9 @@
 from django.db import models
 
 # Create your models here.
-from 
+from author.models import author
+
+class profile(models.Model):
+    name = models.CharField(max_length=100) 
+    about = models.TextField()
+    author = models.OneToOneField(author, on_delete=models.CASCADE, default=None)
