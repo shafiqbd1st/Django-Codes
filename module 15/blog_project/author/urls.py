bb3675bdf0a603cf530a576eea2aefb4fa90410c
shafiqbd1st <shafiqbd1st.com>
2024-03-21 06:49:00 +0000

    Thu, Mar 21, 2024, 12:49 PM +06:00

--- module 15/blog_project/author/urls.py
+++ module 15/blog_project/author/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from .import views
 urlpatterns = [
-    path("add/", views.add_author, name = 'add_author'),
+    path("", views.add_author, name = 'add_author'),
 ]
--- module 15/blog_project/blog_project/urls.py
+++ module 15/blog_project/blog_project/urls.py
@@ -19,8 +19,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-  
+    
     path('', include('author.urls')),
+    path('author/', include('author.urls')),
     path('profile/', include('profiles.urls')),
     path('post/', include('posts.urls')),
     path('category/', include('categories.urls'))
