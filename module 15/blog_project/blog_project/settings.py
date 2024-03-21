7e4dad00b55a51bdfd1d31a0a92abd37c9818639
shafiqbd1st <shafiqbd1st.com>
2024-03-21 06:41:16 +0000

    Thu, Mar 21, 2024, 12:41 PM +06:00

--- module 15/blog_project/blog_project/settings.py
+++ module 15/blog_project/blog_project/settings.py
@@ -41,7 +41,12 @@ INSTALLED_APPS = [
     "categories",
     "posts",
     "profiles",
+    "crispy_forms",
+    "crispy_bootstrap5",
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
