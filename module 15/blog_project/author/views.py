c68034f46a44c1aaf9f5a3c617c69f53dbed348f
shafiqbd1st <shafiqbd1st.com>
2024-03-21 06:37:19 +0000

    Thu, Mar 21, 2024, 12:37 PM +06:00

--- module 15/blog_project/author/views.py
+++ module 15/blog_project/author/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .import forms
 # Create your views here.
 def add_author(request):
-    return render(request, "add_author.html")
+    author_form = forms.AuthorForm()
+    return render(request, "add_author.html", {'form': author_form})
