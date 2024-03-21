d99f03f49d3007a2a9c8b81a2d4bf40f11f14b3f
shafiqbd1st <shafiqbd1st.com>
2024-03-21 04:16:59 +0000

    Thu, Mar 21, 2024, 10:16 AM +06:00

--- module 15/blog_project/posts/models.py
+++ module 15/blog_project/posts/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
 # Create your models here.
+from categories.models import Category
 from author.models import Author
 
-class Profile()
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    category = models.ManyToManyField(Category)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    
