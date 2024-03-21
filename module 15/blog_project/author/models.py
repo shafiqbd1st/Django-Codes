44870659bd288166a68bcd95d56076fcfdb3fa44
shafiqbd1st <shafiqbd1st.com>
2024-03-21 04:08:58 +0000

    Thu, Mar 21, 2024, 10:08 AM +06:00

--- module 15/blog_project/author/models.py
+++ module 15/blog_project/author/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.TextField()
+    phone_no = models.CharField(max_length=12)
+    
