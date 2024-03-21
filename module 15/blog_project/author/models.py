ab49df94a88c581764b9d7630cce7ee09c5b2add
shafiqbd1st <shafiqbd1st.com>
2024-03-21 07:15:34 +0000

    Thu, Mar 21, 2024, 1:15 PM +06:00

--- module 15/blog_project/author/models.py
+++ module 15/blog_project/author/models.py
@@ -6,3 +6,6 @@ class Author(models.Model):
     name = models.CharField(max_length=100)
     bio = models.TextField()
     phone_no = models.CharField(max_length=12)
+
+    def __str__(self):
+        return self.name
