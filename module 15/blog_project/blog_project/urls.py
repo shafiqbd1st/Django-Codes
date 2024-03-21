5be00ec2cf7000d662f59f0c3990856ed47631c2
shafiqbd1st <shafiqbd1st.com>
2024-03-21 06:46:41 +0000

    Thu, Mar 21, 2024, 12:46 PM +06:00

--- module 15/blog_project/blog_project/urls.py
+++ module 15/blog_project/blog_project/urls.py
@@ -19,7 +19,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('author/', include('author.urls')),
+  
+    path('', include('author.urls')),
     path('profile/', include('profiles.urls')),
     path('post/', include('posts.urls')),
     path('category/', include('categories.urls'))
