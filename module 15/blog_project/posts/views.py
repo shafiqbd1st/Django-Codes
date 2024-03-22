100712489451521d646dbc4b5e9f489da238c650
shafiqbd1st <shafiqbd1st.com>
2024-03-22 04:13:34 +0000

    Fri, Mar 22, 2024, 10:13 AM +06:00

--- module 15/blog_project/posts/views.py
+++ module 15/blog_project/posts/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render, redirect
 from . import forms
+from . import models
 
 # Create your views here.
 
 
+def add_post(request):
+    if request.method == "POST":
+        post_form = forms.PostFrom(request.POST)
+        if post_form.is_valid():
+            post_form.save()
+            return redirect("add_post")
+
+    else:
+        post_form = forms.PostFrom()
+        return render(request, "add_post.html", {"form": post_form})
 def add_post(request):
     if request.method == "POST":
         post_form = forms.PostFrom(request.POST)
