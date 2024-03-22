4a96a61ddc8a50af7249a3701764a8423ef7efd2
shafiqbd1st <shafiqbd1st.com>
2024-03-22 04:28:22 +0000

    Fri, Mar 22, 2024, 10:28 AM +06:00

--- module 15/blog_project/posts/views.py
+++ module 15/blog_project/posts/views.py
@@ -26,3 +26,8 @@ def edit_post(request, id):
             return redirect("homepage")
 
     return render(request, "add_post.html", {"form": post_form})
+
+def delete_post(request, id):
+    post = models.Post.objects.get(pk=id)
+    post.delete()
+    return redirect("homepage")
