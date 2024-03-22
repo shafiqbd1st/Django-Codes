f1f05abc5bf2c972fb6fed6552f74aff4d76aac3
shafiqbd1st <shafiqbd1st.com>
2024-03-22 00:46:04 +0000

    Fri, Mar 22, 2024, 6:46 AM +06:00

--- module 15/blog_project/categories/models.py
+++ module 15/blog_project/categories/models.py
@@ -4,3 +4,5 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=100)
     
+    def __str__(self):
+        return self.name
--- module 15/blog_project/categories/views.py
+++ module 15/blog_project/categories/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .import forms
 # Create your views here.
+
+def add_category(request):
+    if request.method == "POST":
+        category_form = forms.CategoryFrom(request.POST)
+        if category_form.is_valid():
+            category_form.save()
+            return redirect("add_category")
+    
+    else:
+        category_form = forms.CategoryFrom()
+        return render(request, "add_category.html", {"form": category_form})
