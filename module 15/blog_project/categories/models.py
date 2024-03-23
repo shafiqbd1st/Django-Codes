59b93217ecf4e354c48be8903444a5828e6c379e
shafiqbd1st <shafiqbd1st.com>
2024-03-23 11:24:29 +0000

    Sat, Mar 23, 2024, 5:24 PM +06:00

--- module 15/blog_project/categories/models.py
+++ module 15/blog_project/categories/models.py
@@ -3,23 +3,7 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=100)
-    FRESHMAN = "FR"
-    SOPHOMORE = "SO"
-    JUNIOR = "JR"
-    SENIOR = "SR"
-    GRADUATE = "GR"
-    YEAR_IN_SCHOOL_CHOICES = {
-        FRESHMAN: "Freshman",
-        SOPHOMORE: "Sophomore",
-        JUNIOR: "Junior",
-        SENIOR: "Senior",
-        GRADUATE: "Graduate",
-    }
-    year_in_school = models.CharField(
-        max_length=2,
-        choices=YEAR_IN_SCHOOL_CHOICES,
-        default=FRESHMAN,
-    )
+   
     
     def __str__(self):
         return self.name
