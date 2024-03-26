8c039e8ebff18fbb4176948f8970379233e28dac
shafiqbd1st <shafiqbd1st.com>
2024-03-26 05:39:34 +0000

    Tue, Mar 26, 2024, 11:39 AM +06:00

--- moudle 17/eight_project/eight_project/settings.py
+++ moudle 17/eight_project/eight_project/settings.py
@@ -37,8 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'first_app',
+    "crispy_forms",
+    "crispy_bootstrap5",
 ]
-
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -54,7 +58,7 @@ ROOT_URLCONF = 'eight_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
