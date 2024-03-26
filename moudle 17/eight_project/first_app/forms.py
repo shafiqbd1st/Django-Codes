f8264db90e085efb752c23273ecb2ca662ea0688
shafiqbd1st <shafiqbd1st.com>
2024-03-26 05:51:06 +0000

    Tue, Mar 26, 2024, 11:51 AM +06:00

--- moudle 17/eight_project/first_app/forms.py
+++ moudle 17/eight_project/first_app/forms.py
@@ -3,4 +3,9 @@ from django .contrib.auth.models import UserCreationForm
 from django import forms
 
 class UserRegisterForm(UserCreationForm):
-    
+    first_name = forms.CharField(widget=forms.TextInput(attrs={"id": "required"}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={"id": "required"}))
+    email = forms.CharField(widget=forms.EmailInput(attrs={"id": "required"}))
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email', '<PASSWORD>', '<PASSWORD>']
